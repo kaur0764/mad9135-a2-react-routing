2c9f25a009cb757e2d7df1a912cf34bd5d44fd1c
JasreetK <90123505+JasreetK@users.noreply.github.com>
2022-10-06 12:06:38 -0400

    Use Geolocation API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 
 function App() {
+  const [lon, setLon] = useState(null);
+  const [lat, setLat] = useState(null);
+
+  if (!navigator.geolocation) {
+    console.log("Geolocation is not supported by your browser");
+  } else {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLon(position.coords.longitude);
+    });
+  }
+
   return (
     <div className="App">
       <Header />
