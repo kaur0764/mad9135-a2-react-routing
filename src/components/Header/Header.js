bd6d8406631ab173a235f66f2c5155ad71a0cd2b
JasreetK <90123505+JasreetK@users.noreply.github.com>
2022-10-06 11:01:58 -0400

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="App-header">
+      <p>React Router</p>
+    </header>
+  );
+}
+export default Header;
---
+++ src/map.service.js
@@ -0,0 +1,13 @@
+const API_TOKEN = "pk.4cca714eb8c4394b1bc25aebc6ad7b4a";
+const BASE_URL = "https://us1.locationiq.com/v1";
+
+export async function getGeolocation(location) {
+  const url = `${BASE_URL}/search.php?key=${API_TOKEN}&q=${location}&format=json`;
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  const data = await response.json();
+  return { lat: data[0].lat, lon: data[0].lon };
+}
